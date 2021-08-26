270f214dba743593b46412efa2de6021a2f4435e
Ryan Woolsey <rjwoolzee@gmail.com>
2021-08-26 03:04:46 -0700

    refactor pdxcg style

--- water_boy/settings.py
+++ water_boy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,7 +73,7 @@ ROOT_URLCONF = 'water_boy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,10 +137,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
--- water_boy/urls.py
+++ water_boy/urls.py
@@ -17,10 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='base.html'), name='base'),
+    path('/', TemplateView.as_view(template_name='base.html'), name='base'),
     path('admin/', admin.site.urls),
     path("api/v1/", include("djoser.urls")),
     path("api/v1/", include("djoser.urls.authtoken")),
-    path("api/v1/", include("product_app.urls"))
+    path("api/v1/", include("product_app.urls")),
+    path("about/", TemplateView.as_view(template_name='about.html'), name='about'),
+    path("home/", TemplateView.as_view(template_name='home.html'), name='home')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
